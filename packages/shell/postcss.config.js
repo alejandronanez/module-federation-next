db4f1a670b652fe10833ea5a6d42bbebe09744ac
Alejandro Nanez <alejonanez@gmail.com>
2020-07-18 20:30:11 -0500

    Update postcss to get the build working

--- packages/shell/postcss.config.js
+++ packages/shell/postcss.config.js
@@ -1,15 +1,15 @@
 const purgecss = [
-  '@fullhuman/postcss-purgecss',
+  "@fullhuman/postcss-purgecss",
   {
-    content: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
+    content: ["./src/components/**/*.js", "./src/pages/**/*.js"],
     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
 ];
 module.exports = {
   plugins: [
-    'postcss-import',
-    'tailwindcss',
-    'autoprefixer',
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    "postcss-import",
+    "tailwindcss",
+    "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   ],
 };
